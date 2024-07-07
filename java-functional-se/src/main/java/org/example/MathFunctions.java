c2fd9046f2d642ff5f576125f48fba1edeed0ee4
JuanPabaz <jpgthf@gmail.com>
2024-07-07 17:00:50 -0500

    Implement new function and predicate sintax

--- java-functional-se/src/main/java/org/example/MathFunctions.java
+++ java-functional-se/src/main/java/org/example/MathFunctions.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
 
     public static void main(String[] args) {
+
         Function<Integer, Integer> square = new Function<Integer, Integer>() {
             @Override
             public Integer apply(Integer x) {
@@ -14,5 +16,16 @@ public class MathFunctions {
 
         System.out.println(square.apply(5));
         System.out.println(square.apply(25));
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+
+        isEven.test(4);
+
+        System.out.println(isOdd.apply(3));
+        System.out.println(isOdd.apply(2));
+        System.out.println(isEven.test(3));
+        System.out.println(isEven.test(4));
     }
 }
