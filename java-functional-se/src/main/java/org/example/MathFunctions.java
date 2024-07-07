7affbca68c42540c7e4b9d1a9edd71fbeb20cee3
JuanPabaz <jpgthf@gmail.com>
2024-07-07 16:56:07 -0500

    My first Function

---
+++ java-functional-se/src/main/java/org/example/MathFunctions.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.function.Function;
+
+public class MathFunctions {
+
+    public static void main(String[] args) {
+        Function<Integer, Integer> square = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer x) {
+                return x*x;
+            }
+        };
+
+        System.out.println(square.apply(5));
+        System.out.println(square.apply(25));
+    }
+}
